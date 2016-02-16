a9e0e0a43a6cf2088a36c4948743645e3b6abbc4
maxopoly <maxseinbot@gmx.de>
2016-02-16 19:20:41 +0100

    Fix server exclusion handling

--- src/vg/civcraft/mc/bettershards/BetterShardsPlugin.java
+++ src/vg/civcraft/mc/bettershards/BetterShardsPlugin.java
@@ -314,7 +314,16 @@ public class BetterShardsPlugin extends ACivMod{
 	}
 	
 	public void sendBungeeUpdateMessage() {
-		mercuryManager.sendBungeeUpdateMessage(db.getAllExclude());
+		List <String> exc = db.getAllExclude();
+		StringBuilder sb = new StringBuilder();
+		for(String s : exc) {
+			sb.append(s+ "|");
+		}
+		if (sb.length() == 0) {
+			mercuryManager.sendBungeeUpdateMessage("");
+			return;
+		}
+		mercuryManager.sendBungeeUpdateMessage(sb.toString().substring(0, sb.length()-1));
 	}
 	
 	public static CombatTagManager getCombatTagManager() {
--- src/vg/civcraft/mc/bettershards/database/DatabaseManager.java
+++ src/vg/civcraft/mc/bettershards/database/DatabaseManager.java
@@ -9,6 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
