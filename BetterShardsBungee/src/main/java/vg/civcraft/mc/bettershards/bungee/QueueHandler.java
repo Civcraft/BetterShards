2db7922ae93d4c2441a3a3454165fcaf522cc69e
Maxopoly <maxopoly@civcraft.co>
2016-07-27 00:00:39 +0200

    Fix pom and general structure

--- BetterShardsBungee/src/main/java/vg/civcraft/mc/bettershards/bungee/QueueHandler.java
+++ BetterShardsBungee/src/main/java/vg/civcraft/mc/bettershards/bungee/QueueHandler.java
@@ -13,7 +13,6 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.scheduler.ScheduledTask;
-import vg.civcraft.mc.bettershards.BetterShardsPlugin;
 import vg.civcraft.mc.mercury.MercuryAPI;
 
 public class QueueHandler {
